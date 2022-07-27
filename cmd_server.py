7ec0f06fde3894537ba34e1b7612a941828cb06f
Ramesh <ramesh@fcodelabs.com>
2022-07-27 17:41:11 +0530

    add leave_from command

--- cmd_server.py
+++ cmd_server.py
@@ -10,13 +10,25 @@ class CMDServer(Thread):
     def run(self) -> None:
         while(True):
             x = input("> ")
-            if x == 'list peers':
+            if x is None:
+                continue
+            x = x.strip()
+            if x == "":
+                continue
+
+            if x == 'list_peers':
                 l = [f'{i}: {j}' for i, j in enumerate(self.node.peers)]
+                if len(l) == 0:
+                    print("No peers.")
                 print('\n'.join(l))
-            elif x == 'disconnect':
+            elif x == 'disconnect' or x == 'exit' or x == 'stop' or x == 'quit' or x == 'q':
                 self.node.disconnect()
                 return
-            elif x.beginswith('join_to '):
+            elif x.startswith('join_to '):
                 pid = int(x.split(' ')[1])
                 peer = self.node.peers[pid]
                 self.node.join_to(peer)
+            elif x.startswith('leave_from '):
+                pid = int(x.split(' ')[1])
+                peer = self.node.peers[pid]
+                self.node.leave_from(peer)
