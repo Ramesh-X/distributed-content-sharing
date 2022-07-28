1f0df799bf35970f7b3e985e49c94d183e6e4aff
Ramesh <ramesh@fcodelabs.com>
2022-07-28 08:04:19 +0530

    search algo

--- file_server.py
+++ file_server.py
@@ -1,5 +1,6 @@
-import random
 from typing import List
+import random
+import re
 
 class FileServer:
     def __init__(self) -> None:
@@ -9,5 +10,8 @@ class FileServer:
             self.file_names = [file_name.strip() for file_name in random.sample(f.readlines(), k)]
     
     def search(self, query: str) -> List[str]:
-        return [f for f in self.file_names if query.lower() in f.lower()]
+        qs = re.split(r'\W+', query.lower())
+        file_words = [re.split(r'\W+', file_name.lower()) for file_name in self.file_names]
+        ids = [i for i in range(len(file_words)) if all(q in file_words[i] for q in qs)]
+        return [self.file_names[i] for i in ids]
         
